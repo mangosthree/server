c30587a0a62fee84195b020a753fe29af38efb01
Yaki Khadafi <ElSolDolLo@gmail.com>
2012-08-15 18:12:01 +0300

    [0096] Fixed up and cleaned up SMSG_CHAR_ENUM, implemented characters reordering
    Signed-off-by: Yaki Khadafi <ElSolDolLo@gmail.com>

--- src/shared/revision_sql.h
+++ src/shared/revision_sql.h
@@ -1,6 +1,6 @@
 #ifndef __REVISION_SQL_H__
 #define __REVISION_SQL_H__
- #define REVISION_DB_CHARACTERS "required_11785_02_characters_instance"
- #define REVISION_DB_MANGOS "required_12091_01_mangos_spell_template"
- #define REVISION_DB_REALMD "required_10008_01_realmd_realmd_db_version"
+ #define REVISION_DB_CHARACTERS "required_0096_xxxxx_01_characters_characters"
+ #define REVISION_DB_MANGOS "required_0083_12090_01_mangos_spell_template"
+ #define REVISION_DB_REALMD "required_0014_xxxxx_01_realmd_account_access"
 #endif // __REVISION_SQL_H__

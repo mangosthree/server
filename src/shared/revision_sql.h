81441da1adef4f7738d3ce9ba01dde4eef974ae4
stfx <stfx@hotmail.de>
2012-08-17 19:41:26 +0200

    [12097] Add Outdoor PvP placeholders and string texts

--- src/shared/revision_sql.h
+++ src/shared/revision_sql.h
@@ -1,6 +1,6 @@
 #ifndef __REVISION_SQL_H__
 #define __REVISION_SQL_H__
- #define REVISION_DB_CHARACTERS "required_0099_xxxxx_01_characters_character_phase_data"
- #define REVISION_DB_MANGOS "required_0124_12094_01_mangos_creature_template"
- #define REVISION_DB_REALMD "required_0014_xxxxx_01_realmd_account_access"
+ #define REVISION_DB_CHARACTERS "required_11785_02_characters_instance"
+ #define REVISION_DB_MANGOS "required_12097_01_mangos_mangos_string"
+ #define REVISION_DB_REALMD "required_10008_01_realmd_realmd_db_version"
 #endif // __REVISION_SQL_H__

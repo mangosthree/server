1cd806c02e181748132d87fd23d903887e36fcf8
Dramacydal <PulLumBerMal@gmail.com>
2013-05-31 13:26:17 +0100

    [12631] Use uint32 for phaseMasks in core and database

--- src/game/GameObject.h
+++ src/game/GameObject.h
@@ -585,7 +585,7 @@ struct GameObjectData
 {
     uint32 id;                                              // entry in gamobject_template
     uint16 mapid;
-    uint16 phaseMask;
+    uint32 phaseMask;
     float posX;
     float posY;
     float posZ;

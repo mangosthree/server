3122f1fcf3e7812db804f205ba45a5b713053d25
sanctum32 <andriuspel@gmail.com>
2015-02-02 21:09:50 +0000

    finally fixed clangs...

--- src/framework/Utilities/UnorderedMapSet.h
+++ src/framework/Utilities/UnorderedMapSet.h
@@ -96,17 +96,16 @@ HASH_NAMESPACE_END
 #  define HASH_NAMESPACE_END }
 using std::hash_map;
 using std::hash_set;
-#elif COMPILER == COMPILER_CLANG
-# if __clang_major__ == 3 && __clang_minor < 7
-#   define UNORDERED_MAP std::tr1::unordered_map
-#   define UNORDERED_SET std::tr1::unordered_set
-# else
-#   define UNORDERED_MAP std::unordered_map
-#   define UNORDERED_SET std::unordered_set
-# endif
+#elif COMPILER == COMPILER_CLANG && __clang_major__ =< 3 && __clang_minor < 7
+#  define UNORDERED_MAP std::tr1::unordered_map
+#  define UNORDERED_SET std::tr1::unordered_set
 #  define HASH_NAMESPACE_START namespace std { namespace tr1 {
 #  define HASH_NAMESPACE_END } }
-# endif
+#elif COMPILER == COMPILER_CLANG && (__clang_major__ > 3 || __clang_major__ == 3 && __clang_minor >= 7)
+#  define UNORDERED_MAP std::unordered_map
+#  define UNORDERED_SET std::unordered_set
+#  define HASH_NAMESPACE_START namespace std { namespace tr1 {
+#  define HASH_NAMESPACE_END } }
 #elif COMPILER == COMPILER_GNU && (__GNUC__ > 4 || __GNUC__ == 4 && __GNUC_MINOR__ >= 3)
 #  define UNORDERED_MAP std::tr1::unordered_map
 #  define UNORDERED_SET std::tr1::unordered_set

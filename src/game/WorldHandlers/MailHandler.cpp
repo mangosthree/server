756224f5b7fbe2ab2e05900354c0825c0be2ce69
Meltie2013 <Meltie2013@users.noreply.github.com>
2021-02-02 14:00:13 -0600

    Adding safeguard to prevent possible duping of gold in mail. (#131)
    (cherry picked from commit https://github.com/mangoszero/server/commit/b339cfbb57075cbd3137744b9f04fbcf2e7ed87f)

--- src/game/WorldHandlers/MailHandler.cpp
+++ src/game/WorldHandlers/MailHandler.cpp
@@ -210,6 +210,13 @@ void WorldSession::HandleSendMail(WorldPacket& recv_data)
         return;
     }
 
+    // safeguard against possible money dupe
+    if (money && COD)
+    {
+        pl->SendMailResult(0, MAIL_SEND, MAIL_ERR_INTERNAL_ERROR);
+        return;
+    }
+
     uint32 cost = items_count ? 30 * items_count : 30;      // price hardcoded in client
 
     uint64 reqmoney = cost + money;

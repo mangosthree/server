53b3153145c6ecf44363915d7c745652ee3a7bf4
Meltie2013 <Meltie2013@users.noreply.github.com>
2023-01-14 11:29:09 -0600

    Improve timezone handling (#62)

--- src/shared/Utilities/Util.h
+++ src/shared/Utilities/Util.h
@@ -81,12 +81,11 @@ float NormalizeOrientation(float o);
  */
 void stripLineInvisibleChars(std::string& src);
 
-/**
- * @brief
- *
- * @param localtime
- */
-std::tm localtime_r(const time_t& time);
+struct tm* localtime_r(const time_t* time, struct tm* result);
+
+time_t LocalTimeToUTCTime(time_t time);
+time_t GetLocalHourTimestamp(time_t time, uint8 hour, bool onlyAfterTime = true);
+tm TimeBreakdown(time_t t);
 
 /**
  * @brief

5c0aba6190e7f3ad706b1c0a73e580ba2d867a9c
meltie2013 <mfrink17@gmail.com>
2023-01-23 08:20:51 -0600

    Drop Legacy OpenSSL support below 1.1.x

--- src/shared/Auth/HMACSHA1.h
+++ src/shared/Auth/HMACSHA1.h
@@ -35,22 +35,21 @@ class BigNumber;
 
 class HMACSHA1
 {
-public:
-    HMACSHA1(uint32 len, uint8 *seed);
-    ~HMACSHA1();
-    void UpdateBigNumber(BigNumber *bn);
-    void UpdateData(const uint8 *data, int length);
-    void UpdateData(const std::string &str);
-    void Finalize();
-    uint8 *ComputeHash(BigNumber *bn);
-    uint8 *GetDigest() { return (uint8*)m_digest; }
-    int GetLength() { return SHA_DIGEST_LENGTH; }
-private:
-#if OPENSSL_VERSION_NUMBER < 0x10100000L
-    HMAC_CTX m_ctx;
-#else
-    HMAC_CTX* m_ctx;
-#endif
-    uint8 m_digest[SHA_DIGEST_LENGTH];
+    public:
+        HMACSHA1(uint32 len, uint8 *seed);
+        ~HMACSHA1();
+        void UpdateBigNumber(BigNumber *bn);
+        void UpdateData(const uint8 *data, int length);
+        void UpdateData(const std::string &str);
+        void Finalize();
+        uint8 *ComputeHash(BigNumber *bn);
+        uint8 *GetDigest() { return (uint8*)m_digest; }
+        int GetLength() { return SHA_DIGEST_LENGTH; }
+
+    private:
+        HMAC_CTX* m_ctx;
+
+        uint8 m_digest[SHA_DIGEST_LENGTH];
 };
+
 #endif

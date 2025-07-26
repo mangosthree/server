79a5e81d4786020910d641240c690ad4a47aaed6
billy1arm <antz@cix.co.uk>
2025-07-26 23:14:12 +0100

    [Eluna] remove files in prep for Submodule addition

--- src/modules/Eluna/docs/ElunaDoc/templates/sidebar.js
+++
@@ -1,5 +0,0 @@
-document.write(`
-{%- for method in current_class.methods %}
-  <a id="{{ current_class.name + ':' + method.name }}" class="fn" href="{{ root(current_class.name + '/' + method.name + '.html') }}">{{ method.name }}</a>
-{%- endfor %}
-`);

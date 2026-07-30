bba744ab741a2e354feb90f2e7c10e6aa34517cc
billy1arm <antz@cix.co.uk>
2026-07-31 00:17:21 +0100

    Rel23 Prep pt1

--- extra/linux/getmangos.sh
+++ extra/linux/getmangos.sh
@@ -1,15 +1,17 @@
 #!/bin/bash
+# SPDX-License-Identifier: GPL-3.0-or-later
+#
 
 ###############################################################################
 # MaNGOS Build Automation Script                                              #
 # Written By: Ryan Ashley                                                     #
 # Updated By: Cedric Servais                                                  #
 # Updated By: Pysis                                                           #
-# Copyright (C) 2014-2025 MaNGOS https://www.getmangos.eu/                        #
+# Copyright (C) 2014-2026 MaNGOS https://www.getmangos.eu/                        #
 #                                                                             #
-# This program is free software; you can redistribute it and/or modify        #
+# This program is free software: you can redistribute it and/or modify        #
 # it under the terms of the GNU General Public License as published by        #
-# the Free Software Foundation; either version 2 of the License, or           #
+# the Free Software Foundation, either version 3 of the License, or           #
 # (at your option) any later version.                                         #
 #                                                                             #
 # This program is distributed in the hope that it will be useful,             #
@@ -18,8 +20,8 @@
 # GNU General Public License for more details.                                #
 #                                                                             #
 # You should have received a copy of the GNU General Public License           #
-# along with this program; if not, write to the Free Software                 #
-# Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA     #
+# along with this program. If not, see                                #
+# <https://www.gnu.org/licenses/>.                                    #
 ###############################################################################
 
 # Global variables
--- src/game/WorldHandlers/DisableMgr.cpp
+++ src/game/WorldHandlers/DisableMgr.cpp
@@ -1,11 +1,11 @@
 /*
- * Copyright (C) 2015-2025 MaNGOS <https://www.getmangos.eu>
+ * Copyright (C) 2015-2026 MaNGOS <https://www.getmangos.eu>
  * Copyright (C) 2008-2015 TrinityCore <http://www.trinitycore.org/>
  * Copyright (C) 2005-2008 MaNGOS <http://getmangos.com/>
  *
- * This program is free software; you can redistribute it and/or modify it
+ * This program is free software: you can redistribute it and/or modify it
  * under the terms of the GNU General Public License as published by the
- * Free Software Foundation; either version 2 of the License, or (at your
+ * Free Software Foundation, either version 3 of the License, or (at your
  * option) any later version.
  *
  * This program is distributed in the hope that it will be useful, but WITHOUT
